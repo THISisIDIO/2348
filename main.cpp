25faeba6c02e23c6764b81a20099892d63d315e3
faddey <faddey.b@mail.ru>
2022-03-11 17:12:39 +0300

    write triples numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-/* 1. запомнить и вывести числа 
+/* 1. + запомнить и вывести числа 
    2. вывести тройки чисел 
    3. вывести сумма троек 
    4. вывести сумму которая не делится на 4 
@@ -14,7 +14,9 @@ int main() {
 	for (int i(0);i<n;i++)
 	    cin >> numbers[i];
 	for (int i(0);i<n;i++)
-	    cout << numbers[i];	
+	for (int j(i+1);j<n;j++)
+	for (int l(j+1);l<n;l++)
+	    cout << numbers[i] << " " << numbers[j] << " " << numbers[l] << endl ;	
 	    
 
 	return 0;
