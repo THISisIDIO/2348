637453f026c4f5b985f2ac6f465563ddf450f335
faddey <faddey.b@mail.ru>
2022-03-11 17:26:15 +0300

    write number of triples

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 /* 1. + запомнить и вывести числа 
    2. + вывести тройки чисел 
    3. + вывести сумма троек 
-   4. вывести сумму которая не делится на 4 
+   4. +вывести сумму которая не делится на 4 
    5. вывести их кол-во
 */
 
 int main() {
 	int n(0);
 	cin >> n;
+	int a(0);
 	vector<int> numbers(n);
 	for (int i(0);i<n;i++)
 	    cin >> numbers[i];
@@ -20,8 +21,9 @@ int main() {
 	{
 		int sum3=numbers[i] + numbers[j] + numbers[l];
 	    if (sum3%4!=0)
-	        cout << numbers[i] + numbers[j] + numbers[l] << endl ;	
+	        a+=1;	
 	}
+	cout << a;
 	    
 	    
 
