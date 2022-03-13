0799d477f4486a703ac82c758b362d19c7f092e6
faddey <mohamedavdoll@gmail.com>
2022-03-13 15:56:55 +0300

    made all combinations

--- main.cpp
+++ main.cpp
@@ -17,8 +17,23 @@ int main() {
 	    	c[cur_num%4] ++;
 		}
 	cout  <<0.5*c[0]*(c[0]-1)*c[1]  <<endl;
-	cout  <<(c[3]*(c[3]-1)*(c[3]-2)) /6<<endl;
-	    
+	cout  <<0.5*c[0]*(c[0]-1)*c[2]  <<endl;
+	cout  <<0.5*c[0]*(c[0]-1)*c[3]  <<endl;
+	cout  <<c[0]*c[1]*c[2]  <<endl;
+	cout  <<0.5*c[1]*(c[1]-1)*c[0]  <<endl;
+	cout  <<c[0]*c[2]*c[3]  <<endl;
+	cout  <<0.5*c[3]*(c[3]-1)*c[0]  <<endl;
+	cout  <<0.5*c[1]*(c[1]-1)*c[3]  <<endl;
+	cout  <<(c[1]*(c[1]-1)*(c[1]-2))/6<<endl;
+	cout  <<0.5*c[2]*(c[2]-1)*c[1]  <<endl;
+	cout  <<c[3]*c[1]*c[2]  <<endl;
+	cout  <<0.5*c[3]*(c[3]-1)*c[1]  <<endl;
+	cout  <<(c[2]*(c[2]-1)*(c[2]-2))/6<<endl;
+	cout  <<0.5*c[2]*(c[2]-1)*c[3]  <<endl;
+	cout  <<0.5*c[3]*(c[3]-1)*c[2]  <<endl;
+	cout  <<(c[3]*(c[3]-1)*(c[3]-2))/6<<endl;
+
+
 
 	return 0;
 }
