641fd76c1ee16c25b962f3b45b02074e587233d2
faddey <faddey.b@mail.ru>
2022-03-15 16:53:29 +0300

    write max sum

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-/* 1. + запомнить и вывести числа 
-   2. + вывести тройки чисел 
-   3. + вывести сумма троек 
-   4. +вывести сумму которая не делится на 4 
+/* 1. + запомнить и вывести числа
+   2. + вывести тройки чисел
+   3. + вывести сумма троек
+   4. +вывести сумму которая не делится на 4
    5. вывести их кол-во
 */
 
 int main() {
 	int n(0);
+	int min1=0;
 	cin >> n;
 	int a(0);
 	vector<int> numbers(n);
@@ -19,13 +20,17 @@ int main() {
 	for (int j(i+1);j<n;j++)
 	for (int l(j+1);l<n;l++)
 	{
-		int sum3=numbers[i] + numbers[j] + numbers[l];
-	    if (sum3%4!=0)
-	        a+=1;	
+	    if (numbers[i]!=numbers[j] and numbers[i]!=numbers[l] and numbers[l]!=numbers[j])
+        {
+            int sum3=numbers[i] + numbers[j] + numbers[l];
+	        if (sum3%4!=0 and sum3>min1)
+	            min1=sum3;
+        }
+
 	}
-	cout << a;
-	    
-	    
+	cout << min1;
+
+
 
 	return 0;
 }
